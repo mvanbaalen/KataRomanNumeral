6746241a6e48e9d07525467a2aed7cac09a6edf2
Michael van Baalen <mystic83@gmail.com>
2016-07-11 00:04:21 -0400

    add personal tests for tdd and sanity checking

---
+++ test/test_arabic_to_roman.py
@@ -0,0 +1,20 @@
+import unittest
+from converter import RomanNumeralConverter as Converter
+
+
+class TestRequirements(unittest.TestCase):
+
+    def test_that_tests_are_running(self):
+        self.assertEqual(True, True)
+
+    def test_that_converter_returns_i_when_given_1(self):
+        self.assertEqual("I", Converter.convert_from_arabic_to_roman(1))
+
+    def test_that_converter_returns_i_when_given_2(self):
+        self.assertEqual("II", Converter.convert_from_arabic_to_roman(2))
+
+    def test_that_converter_returns_iii_when_given_3(self):
+        self.assertEqual("III", Converter.convert_from_arabic_to_roman(3))
+
+if __name__ == '__main__':
+    unittest.main()
