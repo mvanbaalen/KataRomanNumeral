c498f2f416e9c7e0c93c2edd48505ec6f52f14b1
Michael van Baalen <mystic83@gmail.com>
2016-07-11 00:00:04 -0400

    second requirement from kata

--- converter.py
+++ converter.py
@@ -1,4 +1,8 @@
 class RomanNumeralConverter(object):
     @staticmethod
     def convert_from_arabic_to_roman(number):
-        return "I"
+        final_numeral = ""
+        # Naively add Is to match the number
+        for n in range(number):
+            final_numeral += "I"
+        return final_numeral
--- test/test_requirements.py
+++ test/test_requirements.py
@@ -10,5 +10,8 @@ class TestRequirements(unittest.TestCase):
     def test_that_converter_returns_i_when_given_1(self):
         self.assertEqual("I", Converter.convert_from_arabic_to_roman(1))
 
+    def test_that_converter_returns_iii_when_given_3(self):
+        self.assertEqual("III", Converter.convert_from_arabic_to_roman(3))
+
 if __name__ == '__main__':
     unittest.main()
