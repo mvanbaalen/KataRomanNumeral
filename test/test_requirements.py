e9f14c1887554ec24723b5a743a1f8eaf68c9d85
Michael van Baalen <michael@mvanb.com>
2016-07-10 23:26:52 -0400

    create test fixture and verify everything runs

---
+++ test/test_requirements.py
@@ -0,0 +1,11 @@
+import unittest
+import converter
+
+
+class TestRequirements(unittest.TestCase):
+
+    def test_that_tests_are_running(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
