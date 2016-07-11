52d6fc879160d559cf11a3f93ce37f684deca92c
Michael van Baalen <michael@mvanb.com>
2016-07-10 23:50:31 -0400

    first test requirement, remembered to add .gitignore

---
+++ converter.py
@@ -0,0 +1,4 @@
+class RomanNumeralConverter(object):
+    @staticmethod
+    def convert_from_arabic_to_roman(number):
+        return "I"
--- test/test_requirements.py
+++ test/test_requirements.py
@@ -1,11 +1,14 @@
 import unittest
-import converter
+from converter import RomanNumeralConverter as Converter
 
 
 class TestRequirements(unittest.TestCase):
 
     def test_that_tests_are_running(self):
         self.assertEqual(True, True)
+
+    def test_that_converter_returns_i_when_given_1(self):
+        self.assertEqual("I", Converter.convert_from_arabic_to_roman(1))
 
 if __name__ == '__main__':
     unittest.main()
