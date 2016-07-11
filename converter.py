425404ad0999f17f933cc572125df883577bfa44
Michael van Baalen <mystic83@gmail.com>
2016-07-11 00:54:11 -0400

    add together numbers using recursion, testing with a third requirement (1066=MLXVI)

--- converter.py
+++ converter.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+
+
 class RomanNumeralConverter(object):
 
     lookup = {
@@ -10,13 +13,22 @@ class RomanNumeralConverter(object):
         1000: "M"
     }
 
+    # Using this value often, save as constant
+    LARGEST_VALUE = max(lookup.iterkeys())
+
     @classmethod
     def convert_from_arabic_to_roman(cls, number):
-        final_numeral = ""
-        # Naively add Is to match the number
-        if number < 4:
-            for n in range(number):
-                final_numeral += "I"
-        else:
-            return cls.lookup[number]
-        return final_numeral
+        # Use recursion to add numbers together
+
+        # Base case
+        if number == 0:
+            return ""
+
+        # Track the largest digit to use here
+        last_step = cls.LARGEST_VALUE
+        for step in sorted(cls.lookup.keys()):
+            if number < step or number > cls.LARGEST_VALUE:
+                return cls.lookup[last_step] + cls.convert_from_arabic_to_roman(number - last_step)
+            elif number == step:
+                return cls.lookup[step]
+            last_step = step
--- test/test_arabic_to_roman.py
+++ test/test_arabic_to_roman.py
@@ -19,8 +19,17 @@ class TestRequirements(unittest.TestCase):
     def test_that_converter_returns_v_when_given_5(self):
         self.assertEqual("V", Converter.convert_from_arabic_to_roman(5))
 
+    def test_that_converter_returns_vi_when_given_6(self):
+        self.assertEqual("VI", Converter.convert_from_arabic_to_roman(6))
+
+    def test_that_converter_returns_vii_when_given_7(self):
+        self.assertEqual("VII", Converter.convert_from_arabic_to_roman(7))
+
     def test_that_converter_returns_x_when_given_10(self):
         self.assertEqual("X", Converter.convert_from_arabic_to_roman(10))
+
+    def test_that_converter_returns_xvi_when_given_16(self):
+        self.assertEqual("XVI", Converter.convert_from_arabic_to_roman(16))
 
     def test_that_converter_returns_l_when_given_50(self):
         self.assertEqual("L", Converter.convert_from_arabic_to_roman(50))
@@ -33,6 +42,9 @@ class TestRequirements(unittest.TestCase):
 
     def test_that_converter_returns_m_when_given_1000(self):
         self.assertEqual("M", Converter.convert_from_arabic_to_roman(1000))
+
+    def test_that_converter_returns_mlxvi_when_given_1066(self):
+        self.assertEqual("MLXVI", Converter.convert_from_arabic_to_roman(1066))
 
 
 if __name__ == '__main__':
